a7b0664baf7bae510cdfc37217d82ef8109085af
Kal2403 <kal2403199255@gmail.com>
2024-08-31 08:26:14 +0200

    Update components

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import './CSS/ShopCategory.css';
+import { ShopContext } from '../context/ShopContext';
+import dropdown_icon from '../context/ShopContext'
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+
+  const {all_product} = useContext(ShopContext);
+
   return (
-    <div>
-      
+    <div className='shop-category'>
+      <img src={props.banner} alt="" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
 
-export default ShopCategory
+export default ShopCategory;
